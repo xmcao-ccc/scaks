e45d5b851ab7ef0b3d11e35971bd57180fa69368
Xiaoming Cao <101073868+xmcao-ccc@users.noreply.github.com>
2022-03-07 09:20:07 +0800

    Add files via upload

---
+++ README.rst
@@ -0,0 +1,42 @@
+SCAKS
+==========
+
+Shanghai CAtalysis Kinetics Software.  A Micro-Kinetic Analysis package for Catalytic Reactions in Python
+
+Install
+-------
+
+1. Via pip:
+
+   .. code:: shell
+
+      pip install scaks
+
+2. From source
+
+   .. code:: shell
+
+      git clone https://github.com/PytLab/scaks.git
+
+      cd scaks
+      python setup.py install
+
+Test
+----
+
+.. code:: shell
+
+   python setup.py test
+
+or
+
+.. code:: shell
+
+   python -m scaks.tests.test_all
+
+Documentation
+-------------
+
+For more details, see SCAKS documentation:
+https://scaks-docs.readthedocs.io/en/latest/
+
---
+++ setup.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+from scaks import __version__ as version
+
+maintainer = 'Shao-Zheng-Jiang'
+maintainer_email = 'shaozhengjiang@gmail.com'
+author = maintainer
+author_email = maintainer_email
+description = "Micro-Kinetics Analysis package for Catalyst in Python"
+
+with open('README.rst', 'r') as f:
+    long_description = f.read()
+
+install_requires = [
+    'numpy',
+    'scipy',
+    'matplotlib',
+    'sympy',
+    'mpmath',
+    'mpi4py',
+    'prettytable'
+]
+
+license = 'LICENSE'
+
+name = 'scaks'
+platforms = ['linux', 'macos']
+url = ''
+download_url = ''
+
+classifiers = [
+    'Development Status :: 3 - Alpha',
+    'Topic :: Utilities',
+    'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+    'Programming Language :: Python :: 2'
+    'Programming Language :: Python :: 2.7'
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.5',
+]
+
+test_suite = 'scaks.tests.test_all'
+
+setup(author=author,
+      author_email=author_email,
+      description=description,
+      license=license,
+      long_description=long_description,
+      install_requires=install_requires,
+      maintainer=maintainer,
+      name=name,
+      packages=find_packages(),
+      platforms=platforms,
+      url=url,
+      download_url=download_url,
+      version=version,
+      test_suite=test_suite
+  )
+
